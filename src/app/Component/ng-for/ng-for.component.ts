9f1182e853bec57a54a2cf759da130e23b8a1837
jawad <jawad@correctcloud.io>
2025-06-20 11:29:20 +0530

    make the drop list of list

--- src/app/Component/ng-for/ng-for.component.ts
+++ src/app/Component/ng-for/ng-for.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import {NgForOf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ng-for',
   imports: [
-    NgForOf
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
 export class NgForComponent {
  jawad:string[]= ['India','NY','portugal','Russia']
+
+  selectedItem:string = '';
+    ngOnChanges() {
+      console.log('Selected ID:', this.selectedItem);
+    }
+
+  employeeList:any[]=[
+    {empid:1,name:"jawad",email:'jawad@correctcloud.io'},
+    {empid:2,name:"Izhar Ahmed",email:'izhar@correctcloud.io'},
+    {empid:344,name:"awais",email:'awais@correctcloud.io'}
+  ]
+  selectedEmpId: any='';
+
+  onEmployeeSelect() {
+    console.log('Selected Employee ID:', this.selectedEmpId);
+  }
 }
