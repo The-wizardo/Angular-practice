f804d3acd1bd47044360c7272fac182bd43d6c6c
jawad <jawad@correctcloud.io>
2025-06-20 20:00:09 +0530

    added the router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {UserComponent} from './Component/user/user.component';
-import {NgForComponent} from './Component/ng-for/ng-for.component';
+import {NavbarComponent} from './Component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent,NgForComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {UserComponent} from './Component/user/user.component';
+import {NgForComponent} from './Component/ng-for/ng-for.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"User",
+    component:UserComponent
+  },
+  {
+    path:"Ng-For",
+    component:NgForComponent
+  }
+];
