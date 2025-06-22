431ea424f89f3a42c9dc1226595d54bb53a9d28b
jawad <jawad@correctcloud.io>
2025-06-22 20:21:29 +0530

    added the navbar custom

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import {UserComponent} from './Component/user/user.component';
 import {NgForComponent} from './Component/ng-for/ng-for.component';
+import {ControlStatementComponent} from './Component/control-statement/control-statement.component';
 
 export const routes: Routes = [
+  { path: '',
+    redirectTo: 'User',
+    pathMatch: 'full'
+  },
   {
     path:"User",
     component:UserComponent
@@ -10,5 +15,9 @@ export const routes: Routes = [
   {
     path:"Ng-For",
     component:NgForComponent
+  },
+  {
+    path:"Control-Statement",
+    component:ControlStatementComponent
   }
 ];
